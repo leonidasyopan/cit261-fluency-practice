ba7fcba94d89533c021a7d234a6ee391ffc1202b
Leonidas Yopan <leonidasyopan@gmail.com>
2019-10-01 14:39:56 -0300

    first try of the canvas thing

--- js/canvas-width.js
+++ js/canvas-width.js
@@ -1,4 +1,36 @@
-var canvas = document.querySelector('canvas');
+var canvas1 = document.getElementById('firstCanvas');
+var canvas2 = document.getElementById('secondCanvas');
 
-var context = canvas.getContext('2d');
+var c1 = canvas1.getContext('2d');
+var c2 = canvas2.getContext('2d');
+
+// First Wave
+c1.beginPath();
+for ( var i=1; i <= 50 ; i++) {
+    var x = i * 5;
+    var y = 130;
+    var z = 20;           
+    c1.moveTo(x, y);
+    x += 5;
+    c1.lineTo(x, z);
+    x += 5;
+    c1.lineTo(x, y);
+    c1.strokeStyle = "#2980B9";
+    c1.stroke();
+}
+
+// Second Wave
+c2.beginPath();
+for ( var i=1; i <= 50 ; i++) {
+    var x = i * 5;
+    var y = 20;
+    var z = 130;           
+    c2.moveTo(x, y);
+    x += 5;
+    c2.lineTo(x, z);
+    x += 5;
+    c2.lineTo(x, y);
+    c2.strokeStyle = "#3498DB";
+    c2.stroke();
+}
 
