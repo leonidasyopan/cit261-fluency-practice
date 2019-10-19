3aa34b45790af01c6f6c3c508b98c3542de5e23c
Leonidas Yopan <leonidasyopan@gmail.com>
2019-10-18 21:58:45 -0300

    javascript project

---
+++ javascript/quiz-questions.js
@@ -0,0 +1,103 @@
+/* Array holding the questions */
+var quiz = [
+    {
+    question : "What did Rick have that everybody wanted?",
+    options : [
+        "fame",
+        "money",
+        "Letters of Transit",
+        "liquor"
+    ],
+    answer : 2
+    },
+    {
+    question : "Who said 'play it again Sam'?",
+    options : [
+        "No one",
+        "Ilsa",
+        "Rick",
+        "Victor"
+    ],
+    answer : 0
+    },
+    {
+    question : "What color dress was Ilsa wearing when she and Rick were in Paris?",
+    options : [
+        "Green",
+        "Gray",
+        "Red",
+        "Blue"
+    ],
+    answer : 3
+    },
+    {
+    question : "Why did Ilsa leave Rick waiting at the train station?",
+    options : [
+        "She forgot what time the train left",
+        "She got sick",
+        "She was married",
+        "She changed her mind"
+    ],
+    answer : 2
+    },
+    {
+    question : "What line in the movie was the basis of the title of a 1995 Kevin Spacey movie?",
+    options : [
+        "We'll always have Paris",
+        "Round up the usual suspects",
+        "The Germans wore gray",
+        "This is the beginning of a beautiful friendship"
+    ],
+    answer : 1
+    },
+    {
+    question : "Who was reputed to be the first choice to play the part of Rick Blaine?",
+    options : [
+        "John Wayne",
+        "Ronald Reagan",
+        "Cary Grant",
+        "Jimmy Stewart"
+    ],
+    answer : 1
+    },
+    {
+    question : "The role of Sam was originally meant for a female. Name one of the three African American actresses considered for the part.",
+    options : [
+        "Lena Horne",
+        "Pearl Bailey",
+        "Hattie McDaniel",
+        "Dorothy Dandridge"
+    ],
+    answer : 0
+    },
+    {
+    question : "Who is the owner of the Blue Parrot Cafe?",
+    options : [
+        "Sascha",
+        "Ugarte",
+        "Senor Ferrari",
+        "Major Renault"
+    ],
+    answer : 2
+    },
+    {
+    question : "Casablanca takes place during what war?",
+    options : [
+        "World War 1",
+        "Korean War",
+        "Vietnam War",
+        "World War 2"
+    ],
+    answer : 3
+    },
+    {
+    question : "What is the final line in the movie?",
+    options : [
+        "Here's looking at you, kid.",
+        "Round up the usual suspects",
+        "The problems of three little people don't amount to a hill of beans in this crazy world.",
+        "Louis, this could be the beginning of a beautiful friendship"
+    ],
+    answer : 3
+    }
+];
