b2cc7d63d683b0170bb7ab0137162b090d33fb53
Leonidas Yopan <leonidasyopan@gmail.com>
2019-11-01 17:54:59 -0300

    Defined 4 events to use in the project

--- javascript-events/events-application.js
+++ javascript-events/events-application.js
@@ -1,7 +1,8 @@
-let menuButton = document.querySelector(".menu-button");
+/* Lateral Menu handling application */ 
 
-let menu = document.querySelector(".menu");
-
+// Set variables for the elements of the menu
+const menuButton = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
 let menuStatus = false;
 
 menu.style.marginLeft = "-300px";
@@ -10,6 +11,7 @@ function stopDefAction(evt) {
     evt.preventDefault();
 }
 
+// Function for toggling the menu in or out 
 function menuToggle() {    
     if(menuStatus == false) {
         menu.style.marginLeft = "0px";
@@ -22,5 +24,77 @@ function menuToggle() {
     }
 }
 
-menuButton.onclick = menuToggle;
+menuButton.addEventListener("click", menuToggle);
+menuButton.addEventListener("click", stopDefAction);
 
+/* General Elements and Functions */ 
+
+const eventsPlaceholder = document.querySelector("#events-placeholder");
+
+/* Mouse Hovering events */
+
+const hoverEvents = document.querySelector("#event-hover");
+
+function loadHoverEvents() {
+    var output = "<p>Mouse Hovering events</p>";
+    eventsPlaceholder.innerHTML = output;
+}
+
+hoverEvents.addEventListener("click", loadHoverEvents);
+hoverEvents.addEventListener("click", stopDefAction);
+
+/* Media events */
+
+const mediaEvents = document.querySelector("#event-media");
+
+function loadMediaEvents() {
+    var output = "";
+    output += '<video id="video-box" width="320" height="240" controls onpause="pauseMessage()" onplay="playMessage()">';
+    output += '<source src="neymar-goal.mp4" type="video/mp4">';
+    output += 'Your browser does not support the video tag.';
+    output += '</video>';
+    output += '<p id="video-message"></p>'
+
+    eventsPlaceholder.innerHTML = output;
+}
+
+const videoMessage = document.querySelector("#video-message");
+
+function pauseMessage() {
+    var output =  "The video is paused.";
+    videoMessage.innerHTML = output;
+}
+
+function playMessage() {
+    var output =  "The video is palying.";
+    videoMessage.innerHTML = output;
+}
+
+mediaEvents.addEventListener("click", loadMediaEvents);
+mediaEvents.addEventListener("click", stopDefAction);
+
+
+/* Focus and Blur events */
+
+const focusEvents = document.querySelector("#event-focus");
+
+function loadFocusEvents() {
+    var output = "<p>Focus and Blur events</p>";
+    eventsPlaceholder.innerHTML = output;
+}
+
+focusEvents.addEventListener("click", loadFocusEvents);
+focusEvents.addEventListener("click", stopDefAction);
+
+
+/* Keyboard events */
+
+const keyboardEvents = document.querySelector("#event-keyboard");
+
+function loadKeyboardEvents() {
+    var output = "<p>Keyboard events</p>";
+    eventsPlaceholder.innerHTML = output;
+}
+
+keyboardEvents.addEventListener("click", loadKeyboardEvents);
+keyboardEvents.addEventListener("click", stopDefAction);
