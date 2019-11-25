7e88b72a7b6b98f0d7a8c904f1307518f2d82f8a
Leonidas Yopan <leonidasyopan@gmail.com>
2019-11-25 19:51:48 -0300

    API connection working

--- final-project/js/main-application.js
+++ final-project/js/main-application.js
@@ -1,4 +1,5 @@
 /* Makes API connection and stores the JSON info into LocalStorage */
+/*
 var xmlhttp;
 if (window.XMLHttpRequest){
     xmlhttp = new XMLHttpRequest();
@@ -17,18 +18,60 @@ xmlhttp.onreadystatechange = function(){
 }
 xmlhttp.open("GET","https://api.footystats.org/league-matches?key=example&league_id=2012",true);
 xmlhttp.send();
-
+*/
 
 /* Functioms */
 
 function getGamesList(){    
 
-    var games_deserialized = JSON.parse(localStorage.getItem('gamesStored'));
+    /* Saving requests*/
+    /*
+    var url = 'https://api.football-data.org/v2/teams/65/';
+    var xmlhttp = window.XMLHttpRequest
+        ? new XMLHttpRequest()
+        : new ActiveXObject("Microsoft.XMLHTTP");
 
-    var games = games_deserialized.games;
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var jsondata = JSON.parse(xmlhttp.responseText);
+            var footData  = JSON.stringify(jsondata);    
 
-    var output = '';
-    output += '<p>' + games[i].name + '</p>';    
+            localStorage.setItem('manCityTeam', footData);
+        }
+    }
+
+    xmlhttp.open('GET', url);
+    xmlhttp.setRequestHeader("X-Auth-Token", "383412449bc94f34bccb709be3b40dd3");
+    xmlhttp.send();
+    */
+
+    var team_deserialized = JSON.parse(localStorage.getItem('manCityTeam'));
+
+    var squad = team_deserialized.squad;
     
-    document.getElementById("game-name").innerHTML=output;
+    var output = '';
+    for (i = 0; i < squad.length; i++) {
+        var player = squad.i;
+        for (var key in player) {
+            if (player.hasOwnProperty(key)) {
+                output += '<p>' + key + " -> " + player[key] + '</p>';
+            }
+        }
+    }
+
+    
+    /* Listing keys of the oject 
+    for (var key in players) {
+        if (players.hasOwnProperty(key)) {
+            output += '<p>' + key + " -> " + players[key] + '</p>';
+        }
+    }
+    */
+    
+    
+    document.getElementById("player-name").innerHTML=output;
 }
+
+const showPlayerButton = document.querySelector("#show-player");
+
+showPlayerButton.addEventListener("click", getGamesList);
