45e6988b49e3be7e23e73849f979b853bba64dc7
Leonidas Yopan <leonidasyopan@gmail.com>
2020-01-02 18:57:13 -0300

    working on showing full time score

--- final-project/js/main-application.js
+++ final-project/js/main-application.js
@@ -397,6 +397,10 @@ function displayMatches() {
 
     var premierMatches = matchday_deserialized; 
 
+    var matches_deserialized = JSON.parse(localStorage.getItem('PremierMatches'));
+
+    var matchesList = matches_deserialized.matches;
+
     var output = ''; 
     output += '<h2>Matchday ' + currentMatchDay + '</h2>'
     output += '<section>';
@@ -405,9 +409,10 @@ function displayMatches() {
 
     var premierTeams = teams_deserialized.teams;
     
-    output += '<table id="table-matches"><thead><tr><th>Date/Time</th><th>Home</th><th></th><th>X</th><th></th><th>Away</th></tr></thead><tbody>';
+    output += '<table id="table-matches"><thead><tr><th>Date/Time</th><th>Home</th><th></th><th></th><th>X</th><th></th><th></th><th>Away</th></tr></thead><tbody>';
     for (var i=0; i < premierMatches.matches.length; i++){
-        // Convert the time to local time and separate time and day       
+        // Convert the time to local time and separate time and day
+        var matchAllId = premierMatches.matches[i].id       
         var utcDate = premierMatches.matches[i].utcDate;
         var localDate = new Date(utcDate);
         var time = localDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
@@ -421,6 +426,8 @@ function displayMatches() {
         var homeId = premierMatches.matches[i].homeTeam.id;
         var awayURL = '';
         var homeURL = '';
+        var awayScore = '';
+        var homeScore = '';
 
         /* I also felt very good about the trick I created to show the icon/badge of the team of the Match, since this icon was not available in the JSON file of the matches and I didn't want to manually create all the icons for all the Teams. I also knew I already had the information in the JSON file for the Standings, so I just needed to communicate between the two.*/
         for(let i = 0; i < premierTeams.length; i++){
@@ -430,12 +437,26 @@ function displayMatches() {
                 homeURL = premierTeams[i].crestUrl;
             }
         }
+
+        /* Finding Matches Final Results */
+        for(let i = 0; i < matchesList.length; i++){
+            // if(matchAllId == matchesList[i].id) {
+            if(matchesList[i].id) {
+                awayScore = matchesList[i].score.fullTime.homeTeam;
+                homeScore = matchesList[i].score.fullTime.awayTeam;
+            } else {
+                awayScore = matchesList[i].id;
+                homeScore = matchAllId;
+            }
+        }
         
         output += '<tr>';
         output += '<td>' + strDate + ' at ' + time + '</td>';
         output += '<td>' + premierMatches.matches[i].homeTeam.name + '</td>';
         output += '<td><figure class="figure-matches"><img src="' + homeURL + '" alt="' + premierTeams.name + ' Thumb"></figure></td>';
+        output += '<td>' + homeScore + '</td>';
         output += '<td> X </td>';
+        output += '<td>' + awayScore + '</td>';
         output += '<td><figure class="figure-matches"><img src="' + awayURL + '" alt="' + premierTeams.name + ' Thumb"></figure></td>';            
         output += '<td>' + premierMatches.matches[i].awayTeam.name + '</td>';     
         output += '</tr>';
